ced0a6d8316834b7071ce2382ed7169b47a8f40b
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-10-26 14:20:58 +0100

    getOriginalProvider() made general by URI/Path

--- src/main/java/uk/org/taverna/fswrap/WrappedFileSystemProvider.java
+++ src/main/java/uk/org/taverna/fswrap/WrappedFileSystemProvider.java
@@ -22,19 +22,21 @@ import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.FileAttributeView;
 import java.nio.file.spi.FileSystemProvider;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
 
 
 public class WrappedFileSystemProvider extends FileSystemProvider {
 
-	private static final String JAR = "jar";
 	private static final String WRAP = "wrap";
-	private Map<URI, WeakReference<WrappedFileSystem>> cache = new HashMap<URI, WeakReference<WrappedFileSystem>>();
+	private Map<URI, WeakReference<WrappedFileSystem>> cache = Collections.synchronizedMap(new HashMap<URI, WeakReference<WrappedFileSystem>>());
+	private Map<FileSystem, WrappedFileSystem> origToWrappedFs = new WeakHashMap<>();
 	private FileSystemProvider originalProvider;
 	private Listeners listeners = new Listeners();
 
@@ -131,6 +133,16 @@ public class WrappedFileSystemProvider extends FileSystemProvider {
 	public String getScheme() {
 		return WRAP;
 	}
+	
+	protected FileSystemProvider getOriginalProvider(String originalScheme) {
+		for (FileSystemProvider provider : FileSystemProvider
+				.installedProviders()) {
+			if (provider.getScheme().equals(originalScheme)) {
+				return provider;
+			}
+		}
+		throw new IllegalArgumentException("No provider for scheme " + originalScheme);
+	}
 
 	@Override
 	public WrappedFileSystem newFileSystem(URI uri, Map<String, ?> env)
@@ -142,34 +154,20 @@ public class WrappedFileSystemProvider extends FileSystemProvider {
 		return fs;
 	}
 
-	protected URI changeUriScheme(URI uri, String toScheme) {
-		try {
-			return new URI(toScheme, uri.getRawSchemeSpecificPart(), uri.getRawFragment());
-		} catch (URISyntaxException e) {
-			throw new IllegalStateException("Could not transform URI " + uri);
-		}		
-	}
 	
 	protected URI toOrigUri(URI uri) {
-		return changeUriScheme(uri, JAR);
+		return URI.create(uri.getRawPath());
 	}
 
 	protected URI toWrappedUri(URI uri) {
-		return changeUriScheme(uri, WRAP);
+		try {
+			// TODO: Check that #fragment is preserved
+			return new URI(WRAP, uri.toASCIIString(), null);
+		} catch (URISyntaxException e) {
+			throw new IllegalStateException("Could not transform URI " + uri);
+		}	
 	}
 	
-	protected FileSystemProvider getOriginalProvider() {
-		if (originalProvider == null) {
-			for (FileSystemProvider provider : FileSystemProvider
-					.installedProviders()) {
-				if (provider.getScheme().equals(JAR)) {
-					originalProvider = provider;
-				}
-			}
-		}
-		return originalProvider;
-	}
-
 	@Override
 	public WrappedFileSystem getFileSystem(URI uri) {
 		WeakReference<WrappedFileSystem> ref = cache.get(uri);
@@ -185,72 +183,76 @@ public class WrappedFileSystemProvider extends FileSystemProvider {
 	}
 
 	@Override
-	public WrappedPath getPath(URI uri) {
-		Path originalPath = getOriginalProvider().getPath(toOrigUri(uri));		
-		URI base;
-		try {
-			base = new URI(WRAP, uri.getSchemeSpecificPart().replaceAll("!.*", ""), null);
-		} catch (URISyntaxException e) {
-			throw new IllegalArgumentException("Invalid URI " + uri);
-		}
-		return getFileSystem(base).toWrappedPath(originalPath);
+	public WrappedPath getPath(URI uri) {		
+		Path originalPath = getOriginalProvider(uri.getScheme()).getPath(toOrigUri(uri));
+		WrappedFileSystem fs = getFileSystemWrapping(originalPath.getFileSystem());
+		return fs.toWrappedPath(originalPath);
 	}
 
+	private WrappedFileSystem getFileSystemWrapping(FileSystem originalFileSystem) {
+		return origToWrappedFs.get(originalFileSystem);
+	}
+	
 	@Override
 	public SeekableByteChannel newByteChannel(Path path,
 			Set<? extends OpenOption> options, FileAttribute<?>... attrs)
 			throws IOException {
-		SeekableByteChannel byteChannel = getOriginalProvider().newByteChannel(toOriginalPath(path), options, attrs);
+		SeekableByteChannel byteChannel = getOriginalProvider(path).newByteChannel(toOriginalPath(path), options, attrs);
 		listeners.newByteChannel(path, options, attrs, byteChannel);
 		return byteChannel;
 	}
 
+	private FileSystemProvider getOriginalProvider(Path path) {
+		Path p = toOriginalPath(path);
+		return getOriginalProvider(p.toUri().getScheme());
+	}
+	
 	@Override
 	public DirectoryStream<Path> newDirectoryStream(Path dir,
 			Filter<? super Path> filter) throws IOException {
-		return new WrappedDirectoryStream(dir, getOriginalProvider().newDirectoryStream(toOriginalPath(dir), filter));
+		return new WrappedDirectoryStream(dir, getOriginalProvider(dir).newDirectoryStream(toOriginalPath(dir), filter));
 	}
 
 	@Override
 	public void createDirectory(Path dir, FileAttribute<?>... attrs)
 			throws IOException {
-		getOriginalProvider().createDirectory(toOriginalPath(dir), attrs);
+		getOriginalProvider(dir).createDirectory(toOriginalPath(dir), attrs);
 		listeners.createdDirectory(dir, attrs);
 	}
 
 	@Override
 	public void delete(Path path) throws IOException {
-		getOriginalProvider().delete(toOriginalPath(path));
+		getOriginalProvider(path).delete(toOriginalPath(path));
 		listeners.deleted(path);
 	}
 
 	@Override
 	public void copy(Path source, Path target, CopyOption... options)
 			throws IOException {
-		getOriginalProvider().copy(toOriginalPath(source), toOriginalPath(target), options);		
+		getOriginalProvider(source).copy(toOriginalPath(source), toOriginalPath(target), options);		
 		listeners.copied(source, target, options);
 	}
 
 	@Override
 	public void move(Path source, Path target, CopyOption... options)
 			throws IOException {
-		getOriginalProvider().move(toOriginalPath(source), toOriginalPath(target), options);
+		getOriginalProvider(source).move(toOriginalPath(source), toOriginalPath(target), options);
 		listeners.moved(source, target, options);
 	}
 
 	@Override
 	public boolean isSameFile(Path path, Path path2) throws IOException {
-		return getOriginalProvider().isSameFile(toOriginalPath(path), toOriginalPath(path2));
+		return getOriginalProvider(path).isSameFile(toOriginalPath(path), toOriginalPath(path2));
 	}
 
 	@Override
 	public boolean isHidden(Path path) throws IOException {
-		return getOriginalProvider().isHidden(toOriginalPath(path));
+		return getOriginalProvider(path).isHidden(toOriginalPath(path));
 	}
 
 	@Override
 	public FileStore getFileStore(Path path) throws IOException {
-		return new WrappedFileStore(getOriginalProvider().getFileStore(toOriginalPath(path)));
+		return new WrappedFileStore(getOriginalProvider(path).getFileStore(toOriginalPath(path)));
 	}
 
 	@Override
@@ -261,13 +263,13 @@ public class WrappedFileSystemProvider extends FileSystemProvider {
 	@Override
 	public <V extends FileAttributeView> V getFileAttributeView(Path path,
 			Class<V> type, LinkOption... options) {
-		return getOriginalProvider().getFileAttributeView(toOriginalPath(path), type, options);
+		return getOriginalProvider(path).getFileAttributeView(toOriginalPath(path), type, options);
 	}
 
 	@Override
 	public <A extends BasicFileAttributes> A readAttributes(Path path,
 			Class<A> type, LinkOption... options) throws IOException {
-		return getOriginalProvider().readAttributes(toOriginalPath(path), type, options);
+		return getOriginalProvider(path).readAttributes(toOriginalPath(path), type, options);
 	}
 	
 	protected Path toOriginalPath(Path other) {
@@ -285,7 +287,7 @@ public class WrappedFileSystemProvider extends FileSystemProvider {
 	@Override
 	public Map<String, Object> readAttributes(Path path, String attributes,
 			LinkOption... options) throws IOException {
-		return getOriginalProvider().readAttributes(toOriginalPath(path), attributes, options);
+		return getOriginalProvider(path).readAttributes(toOriginalPath(path), attributes, options);
 	}
 
 	@Override
